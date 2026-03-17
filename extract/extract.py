36bbe292b290cf8533d640f7808e7bd81dc74af2
pddabcptitu <phamdinh11012004@gmail.com>
2026-03-17 23:12:31 +0700

    fixed extract.py

--- extract/extract.py
+++ extract/extract.py
@@ -1,23 +1,37 @@
 import torch
 import os
 from extract.video_loader import VideoLoader
+from torch.utils.data import DataLoader
 from extract.model_video_extract import VideoExtract
 
-def extract_and_save(video_paths, model_name='ViT-L-14', output_dim=768, batch_size=128, size=(224, 224), save_dir='features_output', target_fps=2):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+def extract_and_save(
+    video_paths,
+    model_name='ViT-L-14',
+    output_dim=768,
+    batch_size=128,
+    size=(224, 224),
+    save_dir='features_output',
+    target_fps=2
+):
+    os.makedirs(save_dir, exist_ok=True)
 
-    loader = VideoLoader(video_paths, fps=target_fps, save_dir=save_dir)
-    extractor = VideoExtract(model_name=model_name, output_dim=output_dim, batch_size=batch_size, size=size)
+    loader = VideoLoader(video_paths, fps=target_fps)
+    dataloader = DataLoader(loader, batch_size=1, shuffle=False)
 
-    for i, v in enumerate(loader):
-        features = extractor(v) 
+    extractor = VideoExtract(
+        model_name=model_name,
+        output_dim=output_dim,
+        batch_size=batch_size,
+        size=size
+    )
+
+    for i, v in enumerate(dataloader):
+        with torch.no_grad():
+            features = extractor(v)
 
         video_name = os.path.basename(video_paths[i]).split('.')[0]
         save_path = os.path.join(save_dir, f"{video_name}.pt")
 
-        torch.save(features, save_path)
-        
-        # print(f"Đã lưu feature của {video_name} - Shape: {features.shape}")
+        torch.save(features.cpu(), save_path)
 
     print(f"--- Hoàn thành! Toàn bộ feature nằm trong: {save_dir} ---")
